1fe231c827dcbe0d87303453b889ded09d608cee
Sardie <sardaukar1986@hotmail.com>
2014-08-10 23:52:16 +0800

    Second Commit
    Second commit. Now with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,57 @@
-## creates one function that gets or sets the inverse of a matrix from another 
-## function that solves the invertible function passed
+## Creates two functions. The first function features a list of 'get' and 'set'
+## functions and allows the creation of a special matrix object that has these 
+## list of functions as its properties/attributes.
 
-## the function that is a list of 'get' and 'set' functions for the next function
-## to utilise
+## The second function solves for the inverse of the special matrix created in the
+## first function and stores it back in cache; this function assumes that all 
+## matric objects passed into it are invertible matrices.
+
+
+## This function creates a special matrix object that gives special functions 
+## to it. 
+## 1) 'get' allows the user to get the values of the originally stored matrix object
+## 2) 'set' allows the user to set a new value or replace that of the original  
+##    matrix object
+## 3) 'getInv' allows the user to get the inverse values of the stored matrix if  
+##    the inverse has already be stored in cache.
+## 4) 'setInv' allows the user to set the inverse values of the stored matrix after 
+##    the inverse values have been solved by the cacheSolve function. 
+
+## Note:'setInv' should not be called even though it is readily available for 
+## to call as it will not return the actual inverse of the special matrix object 
+## previously created if it was not called using the cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setInv <- function(invMatrix) m <<- invMatrix
-    getInv <- function() m
-    list(set = set, get = get,
-        setInv = setInv,
-        getInv = getInv)
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInv <- function(invMatrix) m <<- invMatrix
+        getInv <- function() m
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## the function that solves the inverse of the matrix passed in
+## This function solves for the inverse of the special matrix object stored in cache
+## It assumes that the matrix passed as argument is invertible
+## This function uses functions from the makeCacheMatrix function to display the 
+## inverse matrix if it is already calculated and cached. Else, it will proceed to
+## get the special matrix stored and cache the inverse for retrieval later.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
-    m <- x$getInv()
-    if(!is.null(m)) {
-       message("getting cached inversed matrix")
-       return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setInv(m)
-    m
+                
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached inversed matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m
 }
